9feb8a9ad91b514ec48436e88b2f8e7deb48cf75
Breno Silva <brenosill@hotmail.com>
2026-03-31 01:45:28 -0300

    feat: Primeira subida

---
+++ src/test/java/com/brenosilldev/pmanager/PmanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brenosilldev.pmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PmanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
